832558fecae29e163ff90acc05ad92f58642c79b
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-08-28 16:05:33 +0200

    Added a second navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
-import styles from './NavBar.module.scss'
+import styles from './NavBar.module.scss';
+import { NavLink } from 'react-bootstrap';
 
 const NavBar = () => {
     return (
@@ -8,12 +9,27 @@ const NavBar = () => {
                 <p>© 2023 NatureEscape, All Rights Reserved</p>
             </div>
             <div className={styles.right}>
-                <p>Ready for a unique experience?</p>
-                <p> +48 765 432 198</p>
-                <p>info@nature-escape.com</p>
+                <p>Ready for a unique tour?</p>
+                <p> <i className="fa-solid fa-phone"></i> +48 765 432 198</p>
+                <p> <i class="fa-solid fa-envelope"></i> info@nature-escape.com</p>
             </div>
         </div>
         <div className={styles.main}></div>
+        <div>
+            <h1>NATURE ESCAPE</h1>
+        </div>
+        <div>
+            <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+                to="/">Home</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+                to="/favorite">Your Cart</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+                to="/about">About</NavLink></li>
+        </div>
+        <div>
+            <i class="fa-solid fa-envelope"></i>
+            <i class="fa-solid fa-envelope"></i>
+        </div>
     </div>
     )
 }
