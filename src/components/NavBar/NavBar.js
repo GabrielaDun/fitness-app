d98b2150bb6cd2f2ab3cdfc25233fa18c7793389
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-08-30 20:55:45 +0200

    Added fonts and icones

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,12 @@
 import styles from './NavBar.module.scss';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-    <div>
+    <div className={styles.container}>
         <div className={styles.info}>
             <div className={styles.left}>
-                <p>© 2023 NatureEscape, All Rights Reserved</p>
+                <p>© 2023 NaturEscape, All Rights Reserved</p>
             </div>
             <div className={styles.right}>
                 <p>Ready for a unique tour?</p>
@@ -15,8 +15,8 @@ const NavBar = () => {
             </div>
         </div>
         <div className={styles.main}>
-            <div>
-                <h1>NATURE ESCAPE</h1>
+            <div className={styles.logo}>
+                NaturEscape
             </div>
             <div className={styles.nav}>
                 <li><NavLink className={ ({ isActive }) =>  isActive ? styles.linkActive : undefined}
@@ -26,9 +26,9 @@ const NavBar = () => {
                 <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
                     to="/about">About</NavLink></li>
             </div>
-            <div>
-                <i className="fa-solid fa-envelope"></i>
-                <i className="fa-solid fa-envelope"></i>
+            <div className={styles.symbols}>
+                <i className="fa-solid fa-basket-shopping"></i>
+                <i className="fa-solid fa-bars"></i>
             </div>
         </div>
     </div>
