0db311fa49753d3a325b61575f230bcfafebb73f
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-11-21 08:18:25 +0100

    create steup for order and itemOrder endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,13 +8,23 @@ import * as cors from 'cors';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ToursModule } from './tours/tours.module';
-import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { CategoriesModule } from './categories/categories.module';
 import { TagsModule } from './tags/tags.module';
+import { OrderItemModule } from './order-items/order-item.module';
+import { OrdersModule } from './orders/orders.module';
+import { ClientsModule } from './clients/clients.module';
 
 @Module({
-  imports: [ToursModule, AuthModule, ConfigModule.forRoot({ isGlobal: true }), CategoriesModule, TagsModule],
+  imports: [
+    ToursModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    CategoriesModule,
+    TagsModule,
+    OrderItemModule,
+    OrdersModule,
+    ClientsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
