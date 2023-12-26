8481f2d2889748b9d71a9b8ab27ce3dc169055c8
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-12-26 08:39:24 +0100

    Made styling improvement

--- client/src/components/layout/NavBar/NavBar.js
+++ client/src/components/layout/NavBar/NavBar.js
@@ -14,51 +14,51 @@ const NavBar = () => {
     const itemCount = useSelector(getTotalCartItems);
 
     return (
-    <div className={styles.container}>
-        <div className={styles.info}>
-            <div className={styles.left}>
-                <p>© 2023 Gabriela Dunowska, All Rights Reserved</p>
+        <div className={styles.container}>
+            <div className={styles.info}>
+                <div className={styles.left}>
+                    <p>© 2023 Gabriela Dunowska, All Rights Reserved</p>
+                </div>
+                <div className={styles.right}>
+                    <p>Ready for a unique tour?</p>
+                    <p> <i className="fa-solid fa-phone"></i> +48 765 432 198</p>
+                    <p className={styles.email}> <i className="fa-solid fa-envelope"></i> info@nature-escape.com</p>
+                </div>
             </div>
-            <div className={styles.right}>
-                <p>Ready for a unique tour?</p>
-                <p> <i className="fa-solid fa-phone"></i> +48 765 432 198</p>
-                <p className={styles.email}> <i className="fa-solid fa-envelope"></i> info@nature-escape.com</p>
+            <div className={styles.main}>
+                <div className={styles.logo}>
+                    <NavLink to="/">
+                    NaturEscape
+                    </NavLink>
+                </div>
+                <div className={styles.nav}>
+                    <li><NavLink className={ ({ isActive }) =>  isActive ? styles.linkActive : undefined}
+                        to="/">Home</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined }
+                        to="/products">Products</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+                        to="/about">About</NavLink></li>
+                    <li>
+                        <NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+                        to="/cart">Cart</NavLink></li>
+                </div>
+                <div className={styles.symbols}>
+                    <NavLink to="/cart">
+                        <i className="fa-solid fa-basket-shopping"></i>
+                        {itemCount > 0 && <span className={styles.itemCount}>{itemCount}</span>}
+                    </NavLink>
+                    <i className={`fa-solid fa-bars ${styles.navIcon}`} onClick={toggleNav}></i>
+                    {isNavVisible && (
+                        <ul className={styles.dropdownMenu}>
+                            <li><NavLink to="/">Home</NavLink></li>
+                            <li><NavLink to="/products">Products</NavLink></li>
+                            <li><NavLink to="/about">About</NavLink></li>
+                            <li><NavLink to="/cart">Cart</NavLink></li>
+                        </ul>
+                    )}
+                </div>
             </div>
         </div>
-        <div className={styles.main}>
-            <div className={styles.logo}>
-                <NavLink to="/">
-                NaturEscape
-                </NavLink>
-            </div>
-            <div className={styles.nav}>
-                <li><NavLink className={ ({ isActive }) =>  isActive ? styles.linkActive : undefined}
-                    to="/">Home</NavLink></li>
-                <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined }
-                    to="/products">Products</NavLink></li>
-                <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
-                    to="/about">About</NavLink></li>
-                <li>
-                    <NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
-                    to="/cart">Cart</NavLink></li>
-            </div>
-            <div className={styles.symbols}>
-                <NavLink to="/cart">
-                    <i className="fa-solid fa-basket-shopping"></i>
-                    {itemCount > 0 && <span className={styles.itemCount}>{itemCount}</span>}
-                </NavLink>
-                <i className={`fa-solid fa-bars ${styles.navIcon}`} onClick={toggleNav}></i>
-                {isNavVisible && (
-                    <ul className={styles.dropdownMenu}>
-                        <li><NavLink to="/">Home</NavLink></li>
-                        <li><NavLink to="/products">Products</NavLink></li>
-                        <li><NavLink to="/about">About</NavLink></li>
-                        <li><NavLink to="/cart">Cart</NavLink></li>
-                    </ul>
-                )}
-            </div>
-        </div>
-    </div>
     )
 }
 
